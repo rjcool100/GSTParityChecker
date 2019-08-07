32056e40210e51ef670899c93a4d5265a3ba5052
rojo <rohit.joshi@i2e1.com>
2019-08-07 11:41:12 +0530

    length check for gst number

--- Util/paritychecker.js
+++ Util/paritychecker.js
@@ -2,6 +2,8 @@ var checksum=require('./checksum');
 var stateChecker=require('./stateChecker');
 
 function check(gstin){
+    if(gstin.length!=15)
+        return false;
     var state=stateChecker.check(gstin.substring(0,2));
     if(!state){
         return false;
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 var parityChecker=require('./Util/paritychecker');
-
-
-console.log(parityChecker.check('34AABB5576G1ZQ'));
+console.log(parityChecker.check('34AABDB5576G1ZQ'));
 
 
